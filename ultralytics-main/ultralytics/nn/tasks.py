62ba80b8f0855ace4136d0b45b7f61675576a1b1
mcflinta <21521901@gm.uit.edu.vna>
2024-10-08 22:14:22 +0700

    update

--- ultralytics-main/ultralytics/nn/tasks.py
+++ ultralytics-main/ultralytics/nn/tasks.py
@@ -60,9 +60,6 @@ from ultralytics.nn.modules import (
     Segment,
     WorldDetect,
     v10Detect,
-    C2f_EMSC,
-    C3k2_EMSC,
-    C3k2_EMSC_ECA,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
