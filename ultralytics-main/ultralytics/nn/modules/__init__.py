69515b6954b8947e9d15d20e8e11ea5308c196ba
mcflinta <21521901@gm.uit.edu.vna>
2024-10-08 16:06:10 +0700

    add file

--- ultralytics-main/ultralytics/nn/modules/__init__.py
+++ ultralytics-main/ultralytics/nn/modules/__init__.py
@@ -56,6 +56,7 @@ from .block import (
     RepVGGDW,
     ResNetLayer,
     SCDown,
+    C2f_EMSC
 )
 from .conv import (
     CBAM,
@@ -156,4 +157,5 @@ __all__ = (
     "C2fCIB",
     "Attention",
     "PSA",
+    "C2f_EMSC",
 )
