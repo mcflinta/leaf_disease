3dd303bb599f0ebd605d53a0abb602cab9cafd47
mcflinta <21521901@gm.uit.edu.vna>
2024-11-16 09:16:14 +0700

    add file

--- ultralytics-main/ultralytics/nn/modules/block.py
+++ ultralytics-main/ultralytics/nn/modules/block.py
@@ -1186,7 +1186,7 @@ class C3k2_EMSCP(C3k2):
     def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
         super().__init__(c1, c2, n, shortcut, g, e)
         self.m = nn.ModuleList(
-            C3k(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck_EMSCP(self.c, self.c, shortcut, g) for _ in range(n)
+            C3k_EMSCP(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck_EMSCP(self.c, self.c, shortcut, g) for _ in range(n)
         )
 
 class C3k_EMSCP(C3k):
