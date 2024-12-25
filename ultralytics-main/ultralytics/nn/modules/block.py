c694ddc62072405e6fad695d1b0ffdd4d2cd9ace
mcflinta <21521901@gm.uit.edu.vna>
2024-12-25 16:00:08 +0700

    add file

--- ultralytics-main/ultralytics/nn/modules/block.py
+++ ultralytics-main/ultralytics/nn/modules/block.py
@@ -1331,18 +1331,18 @@ class C3k2_Faster(C2f):
         """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
         super().__init__(c1, c2, n, shortcut, g, e)
         self.m = nn.ModuleList(
-            C3k_Faster(self.c, self.c, 2, shortcut, g) if c3k else Faster_Block(self.c, self.c, shortcut, g) for _ in range(n)
+            C3k(self.c, self.c, 2, shortcut, g) if c3k else Faster_Block(self.c, self.c, shortcut, g) for _ in range(n)
         )
 
-class C3k_Faster(C3):
-    """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
+# class C3k_Faster(C3):
+#     """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
 
-    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5, k=3):
-        """Initializes the C3k module with specified channels, number of layers, and configurations."""
-        super().__init__(c1, c2, n, shortcut, g, e)
-        c_ = int(c2 * e)  # hidden channels
-        # self.m = nn.Sequential(*(RepBottleneck(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
-        self.m = nn.Sequential(*(Faster_Block(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
+#     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5, k=3):
+#         """Initializes the C3k module with specified channels, number of layers, and configurations."""
+#         super().__init__(c1, c2, n, shortcut, g, e)
+#         c_ = int(c2 * e)  # hidden channels
+#         # self.m = nn.Sequential(*(RepBottleneck(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
+#         self.m = nn.Sequential(*(Faster_Block(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
 
 class Faster_Block_EMA(nn.Module):
     def __init__(self,
